a753eebda9de7c43d88ce4d9cd5671a138dfdb29
Lorraine <lorrainecannom@gmail.com>
2022-10-23 12:37:30 -0400

    SpringData practice

--- spring-11-Data-TableCreation/src/main/resources/data.sql
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -1,2 +1,8 @@
 INSERT INTO EMPLOYEES (id, name)
 VALUES (1,'Mike');
+
+INSERT INTO student(id, first_name, last_name, email)
+VALUES (1,'Mike', 'SMITH','mike@cydeo.com');
+
+INSERT INTO student(id, first_name, last_name, email)
+VALUES (2,'Tom', 'Hanks','Tom@cydeo.com');
