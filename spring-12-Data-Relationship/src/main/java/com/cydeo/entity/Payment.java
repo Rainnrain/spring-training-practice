16d848eba0f2ce292aa1b0f88a01888baec7d048
Lorraine <lorrainecannom@gmail.com>
2022-10-24 14:10:44 -0400

    SpringData practice

---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
@@ -0,0 +1,38 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Status;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name="payments")
+public class Payment {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate createdDate;
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private Status paymentsStatus;
+
+   // @OneToOne(cascade = CascadeType.ALL)// Causes all related data with that object to be deleted if you delete
+    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    @JoinColumn(name="payment_detail_id")
+    private PaymentDetail paymentDetail;
+
+    public Payment(LocalDate createdDate, BigDecimal amount, Status paymentsStatus) {
+        this.createdDate = createdDate;
+        this.amount = amount;
+        this.paymentsStatus = paymentsStatus;
+    }
+}
